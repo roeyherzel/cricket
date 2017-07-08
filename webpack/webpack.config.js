cdeaf904da9e3eae43ec54a5808a4ed425c301e3
Roey Herzel <royeherzel@gmail.com>
2017-07-07 20:18:44 -0700

    first commit all works

---
+++ webpack/webpack.config.js
@@ -0,0 +1,35 @@
+// require("dotenv").config({ silent: true });
+
+// const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: [path.resolve("src/index.jsx")],
+  },
+
+  output: {
+    path: path.resolve("build"),
+    filename: "[name].[hash].js",
+    chunkFilename: "[name].[chunkhash].js",
+    publicPath: "/",
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  plugins: [
+    // new webpack.EnvironmentPlugin(["NODE_ENV"]),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,54 @@
+const webpack           = require("webpack");
+const path              = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const config            = require("./webpack.config");
+
+module.exports = {
+  devtool: "cheap-eval-source-map",
+
+  entry: {
+    app: [
+      "react-hot-loader/patch",
+      ...config.entry.app,
+    ],
+  },
+
+  resolve: config.resolve,
+
+  output: config.output,
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      template: path.resolve("src/index.html"),
+      minify: { collapseWhitespace: true },
+    }),
+    ...config.plugins,
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: "[name]__[local]--[hash:base64:5]",
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true },
+          },
+          "postcss-loader",
+        ],
+      },
+
+      ...config.module.rules,
+    ],
+  },
+};
