6f48764769c02637d0740e8ccb07779d8f6e65ef
Roey Herzel <roeyherzel@gmail.com>
2017-08-24 20:34:48 -0700

    temporary workaround - remove redux compose devltools
    compose devlTools middlewear breaks Safari and FireFox. need to refactor it fot production env anyway.
    so for now redux devtools is removed.

--- src/store/index.js
+++ src/store/index.js
@@ -10,9 +10,11 @@ const middlewares = applyMiddleware(
   loggerMiddleware, // neat middleware that logs actions
 );
 
+// FIXME: compose devtools breaks safari and firefox
 const store = createStore(
   rootReducer,
-  compose(middlewares, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  middlewares
+  // compose(middlewares, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default store;
